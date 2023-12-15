c572e42ce0fcd490c3c93bdda5af8f55799e54b2
Binnukrishna <binnukrishna5233@gmail.com>
2023-12-16 00:52:50 +0530

    copy to clip board added

--- src/components/Magic.jsx
+++ src/components/Magic.jsx
@@ -1,96 +1,104 @@
-import './Magic.scss'
-import { useState,useEffect } from 'react'
-
+import "./Magic.scss";
+import { useState, useEffect } from "react";
 
 const Magic = () => {
+  const [link, setLink] = useState("");
+  const [articleLink, setArticleLink] = useState([]);
+  const [copied,setCopied] = useState("")
 
-const [ link , setLink ] = useState('')
-const [ articleLink, setArticleLink ] = useState([])
+  const handleChange = (e) => {
+    setLink(e.target.value);
+  };
 
+  const handleMagic = () => {
+    if (link.trim() !== "") {
+      //Saving history to the local storage
 
-const handleChange = (e) => {
-    setLink(e.target.value)
-}
+      let history = localStorage.getItem("history");
+      let updatedHistory = [];
 
-const handleMagic = () => {
-        if (link.trim() !== '') {
+      if (history) {
+        updatedHistory = JSON.parse(history);
+        updatedHistory.push(link);
 
-            
-            //Saving history to the local storage
+        if (updatedHistory.length > 5) {
+          updatedHistory = updatedHistory.slice(updatedHistory.length - 5);
+        }
+      } else {
+        updatedHistory = [link];
+      }
 
+      //Retrieving the history from the local storage
 
-            let history = localStorage.getItem('history');
-            let updatedHistory = [];
-      
-            if (history) {
-              updatedHistory = JSON.parse(history);
-              updatedHistory.push(link);
-      
-              if (updatedHistory.length > 5) {
-                updatedHistory = updatedHistory.slice(updatedHistory.length - 5);
-              }
-            } else {
-              updatedHistory = [link];
-            }
-      
-        //Retrieving the history from the local storage
+      localStorage.setItem("history", JSON.stringify(updatedHistory));
+      setArticleLink(updatedHistory);
+    }
+  };
 
-            localStorage.setItem('history', JSON.stringify(updatedHistory));
-            setArticleLink(updatedHistory);
-          }
-}
+  const handleCopy = (copyLink) => {
+    setCopied("true")
+    navigator.clipboard.writeText(copyLink)
+    setTimeout(()=>{
+        setCopied("false")
+    },1000)
+  };
 
-const handleCopy = () => {
-
-}
-
-useEffect(()=>{
-    const storedHistory = localStorage.getItem('history');
-    if( storedHistory !== null )
-    setArticleLink(JSON.parse(storedHistory));
-},[])
-
+  useEffect(() => {
+    const storedHistory = localStorage.getItem("history");
+    if (storedHistory !== null) setArticleLink(JSON.parse(storedHistory));
+  }, []);
 
   return (
     <>
-        <section>
-            <div className="magic_container">
-                <div className="magic_input">
-                    <div className="link_img">
-                        <i className ="fa-solid fa-link"></i>
-                    </div>
-                    <input type="text" placeholder='Paste the article link.' onChange={ handleChange }/>
-                    <button onClick={ handleMagic } disabled={ link.trim() === '' }>
-                        <i className="fa-solid fa-angle-right"></i>
-                    </button>
-                </div>
-
-                {
-                    articleLink.map(( item,index )=>(
-                        <div key={ index } className="magic_history">
-                            <button onClick={ handleCopy }>
-                                <i className="fa-regular fa-copy"></i>
-                                <i className="fa-solid fa-check" style={{ display:'none' }}></i>
-                            </button>
-                            <input type="text" value={ item } readOnly/>
-                        </div>
-                    ))
-                }
-
-                <div className="magic_summery">
-                    <div className="magic_sum_title">
-                        <h4>Article <span>Summery</span></h4>
-                    </div>
-                    <div className="magic_sum_article">
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere temporibus soluta nulla perspiciatis animi veniam, culpa recusandae voluptatibus et quas corporis perferendis dolor, eveniet molestiae voluptatem deserunt harum quos eum sit dicta dolorum eligendi consectetur? Mollitia delectus distinctio aperiam deserunt praesentium repudiandae molestiae, tempora voluptates sit odit alias omnis unde.
-                        </p>
-                    </div>
-                </div>
+      <section>
+        <div className="magic_container">
+          <div className="magic_input">
+            <div className="link_img">
+              <i className="fa-solid fa-link"></i>
             </div>
-        </section>
-    </>
-  )
-}
+            <input
+              type="text"
+              placeholder="Paste the article link."
+              onChange={handleChange}
+            />
+            <button onClick={handleMagic} disabled={link.trim() === ""}>
+              <i className="fa-solid fa-angle-right"></i>
+            </button>
+          </div>
 
-export default Magic
+          {articleLink.map((item, index) => (
+            <div key={index} className="magic_history">
+              <button onClick={()=>{handleCopy( item )}}>
+                {
+                   copied === "false" ?  <i className="fa-regular fa-copy"></i> : <i className="fa-solid fa-check"></i>
+                }
+              </button>
+              <input type="text" value={item} readOnly />
+            </div>
+          ))}
+
+          <div className="magic_summery">
+            <div className="magic_sum_title">
+              <h4>
+                Article <span>Summery</span>
+              </h4>
+            </div>
+            <div className="magic_sum_article">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
+                temporibus soluta nulla perspiciatis animi veniam, culpa
+                recusandae voluptatibus et quas corporis perferendis dolor,
+                eveniet molestiae voluptatem deserunt harum quos eum sit dicta
+                dolorum eligendi consectetur? Mollitia delectus distinctio
+                aperiam deserunt praesentium repudiandae molestiae, tempora
+                voluptates sit odit alias omnis unde.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Magic;
